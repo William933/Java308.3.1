af8a1a9cb8f9114d7220d13b3ec603c723bb0094
William Allen <williamallen933@gmail.com>
2024-04-14 15:55:25 -0400

    logged fizz

---
+++ READme.md
@@ -0,0 +1,3 @@
+Looped '1-100 looks like the exaample in the exercise from class
+
+was able to print diviends of 3 with for and if statements
---
+++ script.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <=100; i++){
+    console.log(i)
+}
+
+let mer = "Fizz"
+for (let i = 3; i <= 100; i++){
+    if (i % 3 === 0)
+    console.log(mer)
+}
